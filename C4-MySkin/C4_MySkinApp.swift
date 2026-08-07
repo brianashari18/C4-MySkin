0b25f2386e11cae8b9d97fdfdd77eec521af2038
Brian Anashari <brianashari18@gmail.com>
2026-08-07 16:40:09 +0700

    Initial Commit

---
+++ C4-MySkin/C4_MySkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  C4_MySkinApp.swift
+//  C4-MySkin
+//
+//  Created by Brian Anashari on 07/08/26.
+//
+
+import SwiftUI
+
+@main
+struct C4_MySkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
